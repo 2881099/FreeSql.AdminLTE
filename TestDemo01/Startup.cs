332cb314276f94529da5c1d8e26b3aa017605fe2
28810 <28810@YEXIANGQIN>
2019-05-28 22:09:49 +0800

    小包引用

--- TestDemo01/Startup.cs
+++ TestDemo01/Startup.cs
@@ -15,7 +15,6 @@ namespace TestDemo01 {
 
 			Fsql = new FreeSql.FreeSqlBuilder()
 				.UseConnectionString(FreeSql.DataType.Sqlite, @"Data Source=|DataDirectory|/document.db;Pooling=true;Max Pool Size=10")
-				.UseLogger(loggerFactory.CreateLogger<IFreeSql>())
 				.UseAutoSyncStructure(true)
 				.UseLazyLoading(true)
 
@@ -35,10 +34,6 @@ namespace TestDemo01 {
 		}
 
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
-			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-			Console.OutputEncoding = Encoding.GetEncoding("GB2312");
-			Console.InputEncoding = Encoding.GetEncoding("GB2312");
-
 			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 			loggerFactory.AddDebug();
 
